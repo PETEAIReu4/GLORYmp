964b090795dd618f8cc13bacf1c9a31b8c2bd493
Simen Fjeld-Olsen <simen.fjeldolsen@gmail.com>
2020-08-15 11:28:37 +0200

    Updated stuff to 1.30
    Buildings, defines, government, on_action, static_modifiers, interface, missions.

--- common/defines/00_GLRY.lua
+++ common/defines/00_GLRY.lua
@@ -5,7 +5,7 @@ NDefines.NMilitary.GARRISON_SIZE = 2000 									-- [1000]
 NDefines.NMilitary.SIEGE_FORCE_NEEDED_MULTIPLIER = 1 						-- [3] | actually means you'll need 2x the garrison to siege
 NDefines.NCountry.PS_ASSAULT = 0											-- [5]
 NDefines.NCountry.PS_ARTILLERY_BARRAGE = 25									-- [50]
-NDefines.NCountry.PS_NAVAL_BARRAGE = 25										-- [50]
+NDefines.NCountry.PS_NAVAL_BARRAGE = 20										-- [50]
 
 --- WARSCORE ---
 NDefines.NMilitary.DEFAULT_WARGOAL_TICKINGWARSCORE_BONUS = 0.83				-- [0.4]
